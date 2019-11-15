60e584f7a83f4848573173c8b612641feadcecc3
erelender <20099086+erelender@users.noreply.github.com>
2019-11-15 16:43:16 +0300

    Update SybaseDB.js
    Modified parsing jar output stream to support turkish ISO-8859-9 encoding.

--- src/SybaseDB.js
+++ src/SybaseDB.js
@@ -1,9 +1,9 @@
 var spawn = require('child_process').spawn;
-var JSONStream = require('JSONStream');
 var fs = require("fs");
 var path = require("path");
+var iconv = require("iconv-lite");
 
-function Sybase(host, port, dbname, username, password, logTiming, pathToJavaBridge)
+function Sybase(host, port, dbname, username, password, enableLogs, pathToJavaBridge)
 {
     this.connected = false;
     this.host = host;
@@ -11,7 +11,7 @@ function Sybase(host, port, dbname, username, password, logTiming, pathToJavaBri
     this.dbname = dbname;
     this.username = username;
     this.password = password;    
-    this.logTiming = (logTiming == true);
+    this.enableLogs = (enableLogs == true);
     
     this.pathToJavaBridge = pathToJavaBridge;
     if (this.pathToJavaBridge === undefined)
@@ -21,8 +21,6 @@ function Sybase(host, port, dbname, username, password, logTiming, pathToJavaBri
 
     this.queryCount = 0;
     this.currentMessages = {}; // look up msgId to message sent and call back details.
-
-    this.jsonParser = JSONStream.parse();
 }
 
 Sybase.prototype.connect = function(callback)
@@ -42,7 +40,7 @@ Sybase.prototype.connect = function(callback)
 		that.connected = true;
 
 		// set up normal listeners.		
-		that.javaDB.stdout.setEncoding('utf8').pipe(that.jsonParser).on("data", function(jsonMsg) { that.onSQLResponse.call(that, jsonMsg); });
+		that.javaDB.stdout.on("data", function(buf) { that.onSQLResponse.call(that, buf); });
 		that.javaDB.stderr.on("data", function(err) { that.onSQLError.call(that, err); });
 
 		callback(null, data);
@@ -58,8 +56,8 @@ Sybase.prototype.connect = function(callback)
 
 Sybase.prototype.disconnect = function()
 {
-	this.javaDB.kill();
-	this.connected = false;	
+    this.javaDB.kill();
+    this.connected = false;	
 }
 
 Sybase.prototype.isConnected = function() 
@@ -85,48 +83,52 @@ Sybase.prototype.query = function(sql, callback)
     msg.callback = callback;
     msg.hrstart = hrstart;
 
-    console.log("this: " + this + " currentMessages: " +  this.currentMessages + " this.queryCount: " + this.queryCount);
+    if(this.enableLogs)
+        console.log("this: " + this + " currentMessages: " +  this.currentMessages + " this.queryCount: " + this.queryCount);
     
     this.currentMessages[msg.msgId] = msg;
 
     this.javaDB.stdin.write(strMsg + "\n");
-    console.log("sql request written: " + strMsg);
+    if(this.enableLogs)
+        console.log("SQL request written: " + strMsg);
 };
 
-Sybase.prototype.onSQLResponse = function(jsonMsg)
+Sybase.prototype.onSQLResponse = function(buffer)
 {
+    var jsnMsg = JSON.parse(iconv.decode(jsonMsg, 'iso88599'));
+	
     var err = null;
-	var request = this.currentMessages[jsonMsg.msgId];
-	delete this.currentMessages[jsonMsg.msgId];
+    var request = this.currentMessages[jsonMsg.msgId];
+    delete this.currentMessages[jsonMsg.msgId];
 
-	var result = jsonMsg.result;
-	if (result.length === 1)
-		result = result[0]; //if there is only one just return the first RS not a set of RS's
+    var result = jsonMsg.result;
+    if (result.length === 1)
+        result = result[0]; //if there is only one just return the first RS not a set of RS's
 
-	var currentTime = (new Date()).getTime();
-	var sendTimeMS = currentTime - jsonMsg.javaEndTime;
-	hrend = process.hrtime(request.hrstart);
-	var javaDuration = (jsonMsg.javaEndTime - jsonMsg.javaStartTime);
+    var currentTime = (new Date()).getTime();
+    var sendTimeMS = currentTime - jsonMsg.javaEndTime;
+    hrend = process.hrtime(request.hrstart);
+    var javaDuration = (jsonMsg.javaEndTime - jsonMsg.javaStartTime);
 
     if (jsonMsg.error !== undefined)
         err = new Error(jsonMsg.error);
 
 
-	if (this.logTiming)
-		console.log("Execution time (hr): %ds %dms dbTime: %dms dbSendTime: %d sql=%s", hrend[0], hrend[1]/1000000, javaDuration, sendTimeMS, request.sql);
-	request.callback(err, result);
+    if (this.enableLogs)
+        console.log("Execution time (hr): %ds %dms dbTime: %dms dbSendTime: %d sql=%s", hrend[0], hrend[1]/1000000, javaDuration, sendTimeMS, request.sql);
+    request.callback(err, result);
 };
 
 Sybase.prototype.onSQLError = function(data)
 {
-	var error = new Error(data);
+    var error = new Error(data);
 
     var callBackFuncitons = [];
-	for (var k in this.currentMessages){
-    	if (this.currentMessages.hasOwnProperty(k)) { 		
+    for (var k in this.currentMessages){
+	if (this.currentMessages.hasOwnProperty(k)) { 		
             callBackFuncitons.push(this.currentMessages[k].callback);
-    	}
-	}
+        }
+    }
 
     // clear the current messages before calling back with the error.
     this.currentMessages = [];
